31a50f6138618859efe84282427033b05aa41201
hotire <gngh0101@gmail.com>
2020-04-16 00:58:47 +0900

    Implement MonitorAspect

---
+++ src/main/java/com/github/hotire/spring/utils/log/MonitorAspect.java
@@ -0,0 +1,68 @@
+package com.github.hotire.spring.utils.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import java.util.Objects;
+
+
+@Slf4j
+@Aspect
+@Component
+public class MonitorAspect {
+
+    interface Logger {
+        void log(final String format, Object... args);
+    }
+
+    public enum Level implements Logger {
+        DEBUG;
+        @Override
+        public void log(String format, Object... args) {
+            log.debug(format, args);
+        }
+    }
+
+    @Around("@annotation(monitor)")
+    public Object monitorMethod(ProceedingJoinPoint joinPoint, Monitor monitor) throws Throwable {
+        return monitor(joinPoint, monitor);
+    }
+
+    @Around("@within(monitor)")
+    public Object monitorType(ProceedingJoinPoint joinPoint, Monitor monitor) throws Throwable {
+        return monitor(joinPoint, monitor);
+    }
+
+    protected Object monitor(ProceedingJoinPoint joinPoint, Monitor monitor) throws Throwable {
+        final MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        final StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+
+        log.info("monitor :{}", monitor);
+        log.info("Monitor Start");
+        log.info("method : {}", methodSignature.getMethod());
+
+        for (Object arg : joinPoint.getArgs()) {
+            if (Objects.nonNull(arg)) {
+                log.info("arg type : {} value : {}", arg.getClass().getSimpleName(), arg);
+            }
+        }
+
+        final Object result = joinPoint.proceed();
+
+        stopWatch.stop();;
+
+        log.info("result : {}", result);
+        log.info("total elapsed time : {}", stopWatch.getTotalTimeSeconds());
+        log.info("Monitor End");
+
+        return result;
+    }
+
+}
