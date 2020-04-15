6545bca9ee79e03f15f70b8471aa756395173ebf
hotire <gngh0101@gmail.com>
2020-04-15 23:08:36 +0900

    init

---
+++ src/test/java/com/github/hotire/spring/utils/SpringUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hotire.spring.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
