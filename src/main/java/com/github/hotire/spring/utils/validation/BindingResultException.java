18859776683c054bf2e6fb0ab4034ce86e1b3613
hotire <gngh0101@gmail.com>
2020-04-26 02:47:58 +0900

    Add BindingResultException

---
+++ src/main/java/com/github/hotire/spring/utils/validation/BindingResultException.java
@@ -0,0 +1,26 @@
+package com.github.hotire.spring.utils.validation;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.Errors;
+
+import javax.validation.ValidationException;
+
+import static java.util.stream.Collectors.joining;
+
+public class BindingResultException extends ValidationException {
+
+  private final Errors errors;
+
+  public BindingResultException(final Errors errors) {
+    super(errors.getAllErrors()
+      .stream()
+      .map(DefaultMessageSourceResolvable::getDefaultMessage)
+      .distinct()
+      .collect(joining()));
+    this.errors = errors;
+  }
+
+  public Errors getErrors() {
+    return this.errors;
+  }
+}
