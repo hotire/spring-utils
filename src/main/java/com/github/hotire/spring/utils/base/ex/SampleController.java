7aeb7abcf6ec58215a9f029fb6b2d48445ec5774
hotire <gngh0101@gmail.com>
2020-05-24 22:58:34 +0900

    Use bean instead of instance

--- src/main/java/com/github/hotire/spring/utils/base/ex/SampleController.java
+++ src/main/java/com/github/hotire/spring/utils/base/ex/SampleController.java
@@ -3,12 +3,15 @@ package com.github.hotire.spring.utils.base.ex;
 import com.github.hotire.spring.utils.base.EntityController;
 import com.github.hotire.spring.utils.base.EntityRestController;
 import com.github.hotire.spring.utils.base.EntityService;
+import lombok.RequiredArgsConstructor;
 
 @EntityRestController(path = "/sample")
+@RequiredArgsConstructor
 public class SampleController extends EntityController<Account> {
+    private final AccountService accountService;
 
     @Override
     protected EntityService getService() {
-        return new AccountService();
+        return accountService;
     }
 }
