93f8cffa10ce93de72e2a95ccdcaf93f5a04350d
hotire <gngh0101@gmail.com>
2020-06-16 13:26:25 +0900

    Add final to id

--- src/main/java/com/github/hotire/spring/utils/base/Entity.java
+++ src/main/java/com/github/hotire/spring/utils/base/Entity.java
@@ -1,8 +1,10 @@
 package com.github.hotire.spring.utils.base;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class Entity {
-    private Long id;
+    private final Long id;
 }
