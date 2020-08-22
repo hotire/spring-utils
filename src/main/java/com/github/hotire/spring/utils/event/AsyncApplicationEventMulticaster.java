a2e2989e6b33fae92442240b586a010d97f227e4
hotire <gngh0101@gmail.com>
2020-08-22 13:01:02 +0900

    Add AsyncApplicationEventMulticaster

---
+++ src/main/java/com/github/hotire/spring/utils/event/AsyncApplicationEventMulticaster.java
@@ -0,0 +1,6 @@
+package com.github.hotire.spring.utils.event;
+
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+
+public class AsyncApplicationEventMulticaster extends SimpleApplicationEventMulticaster {
+}
