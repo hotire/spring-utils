429253990f7dace643c5c693a322ec499539250c
hotire <gngh0101@gmail.com>
2020-08-23 12:43:19 +0900

    Add constructor of executor to AsyncApplicationEventMulticaster

--- src/main/java/com/github/hotire/spring/utils/event/AsyncApplicationEventMulticaster.java
+++ src/main/java/com/github/hotire/spring/utils/event/AsyncApplicationEventMulticaster.java
@@ -2,5 +2,11 @@ package com.github.hotire.spring.utils.event;
 
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 
+import java.util.Objects;
+import java.util.concurrent.Executor;
+
 public class AsyncApplicationEventMulticaster extends SimpleApplicationEventMulticaster {
+    public AsyncApplicationEventMulticaster(final Executor executor) {
+        setTaskExecutor(Objects.requireNonNull(executor));
+    }
 }
